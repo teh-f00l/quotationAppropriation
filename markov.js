c707991eb6117a21d2ea8065f209e72a37b0e085
Jennifer Currie <renestl@gmail.com>
2018-11-16 18:03:08 -0600

    Refactor markov_byLetter

--- markov.js
+++ markov.js
@@ -4,12 +4,24 @@ let order = 3;
 let ngrams = {};
 let quoteMin = 10;
 let quoteMax = 120;
-let randomLength = getRandomQuoteLength();
+let beginnings = [];
 
-getGrams();
-markovText();
+// Length of final quote
+let getRandomQuoteLength = () => {
+	let min = Math.ceil(quoteMin);
+	let max = Math.floor(quoteMax);
+	return Math.floor(Math.random() * (max - min + 1)) + min;;
+}
 
-function getGrams() {
+// Split text into letters
+let createBeginnings = () => {
+	for (var j = 0; j < text.length; j++) {
+		beginnings.push(text.charAt(j));
+	}
+}
+
+// Gather text for markov object
+let getGrams = () => {
 	for (var i = 0; i <= text.length - order ; i++) {
 		var gram = text.substring(i, i + order);
 
@@ -20,26 +32,39 @@ function getGrams() {
 	}
 }
 
-function markovText() {
-	let currentGram = text.substring(0, order);
-	let result = currentGram;
-
-	for (var i = 0; i < randomLength; i++) {
-		let possiblities = ngrams[currentGram];
-		let next = possiblities[Math.floor(Math.random() * possiblities.length)];
-		result += next;
-		let len = result.length
-		currentGram = result.substring(len - 3, len);
-	}
-	
-	// console.log(`${result}.`);
-	return `${result}.`;
+// Return random token from array
+let chooseStartingToken = () => {
+	let index = Math.floor(Math.random() * beginnings.length);
+	return beginnings[index];
 }
 
-function getRandomQuoteLength() {
-	let min = Math.ceil(quoteMin);
-	let max = Math.floor(quoteMax);
-	return Math.floor(Math.random() * (max - min + 1)) + min;;
+// Select random letter from possibilities
+let findNextLetter = (currentGram) => {
+	let nextLetters = []; // creates list of letters after current letter
+	for (var l = 0; l < beginnings.length; l++) {
+		if(beginnings[l] === currentGram) {
+			nextLetters.push(beginnings[l+1]);
+		}
+	}
+
+	let letter = nextLetters[Math.floor(Math.random() * nextLetters.length)];
+	return letter;
+}
+
+let markovText = () => {
+	createBeginnings();
+	getGrams();
+	
+	let quoteLength = getRandomQuoteLength();
+	let currentGram = chooseStartingToken();
+	let result = currentGram;
+
+	while (result.length < quoteLength) {
+		currentGram = findNextLetter(currentGram);
+		result += `${currentGram}`;
+	}
+
+	return result;
 }
 
 module.exports = markovText();
